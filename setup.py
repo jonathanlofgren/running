224977e4cd6256a31095b9970e98df39dcda1eea
Jonathan Lofgren <lofgren021@gmail.com>
2022-11-27 18:59:56 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
 from setuptools import setup
+import os, sys
+
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == "publish":
+    os.system("python -m build")
+    os.system("twine upload dist/*")
+    sys.exit()
+
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == "publish-test":
+    os.system("python -m build")
+    os.system("twine -r testpypi upload dist/*")
+    sys.exit()
+
+with open("README.md", "r") as f:
+    readme = f.read()
 
 setup(
     name='running',
-    version='0.1.2',
+    version='0.1.3',
     description='User-friendly running pace calculator',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     url='https://github.com/jonathanlofgren/running',
     author='Jonathan Löfgren',
     author_email='lofgren021@gmail.com',
